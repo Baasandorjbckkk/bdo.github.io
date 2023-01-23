e763f96aedb9b4ba2390f18f9afca3f59527f5ef
Dugartogtokh <togtohdugar@gmail.com>
2023-01-23 21:45:14 +0800

    JS1 duusaw

---
+++ travel.js
@@ -0,0 +1,43 @@
+getData();
+
+function getData() {
+    let data = new Array();
+
+    fetch('./travelCountry.json', {
+        method: 'GET',
+        headers: {
+            'Accept': 'application/json',
+        },
+    })
+    .then(response => response.json())
+    .then(response => {
+        data = response;
+        addElement(data);
+    }).catch()
+}
+
+function addElement(items) {
+    for (let i = 0; i < items.length; i ++) {
+        let artElem = document.createElement("article");
+        let divElem = document.createElement("div");
+        let divElem2 = document.createElement("div");
+        let h3Elem = document.createElement("h3");
+        let pElem = document.createElement("p")
+        let img = document.createElement("img");
+        // attr1.value = items[0].path;
+        divElem.setAttribute("class", "container");
+        divElem2.setAttribute("class", "details");
+        artElem.setAttribute("class", "card");
+
+        divElem2.appendChild(h3Elem);
+        divElem2.appendChild(pElem);
+        
+        h3Elem.innerText = items[i].name;
+        pElem.innerText = items[i].description;
+        img.setAttribute("src", items[i].path);
+        divElem.appendChild(img);
+        divElem.appendChild(divElem2);
+        artElem.appendChild(divElem);
+        document.getElementById("travelCountry").appendChild(artElem);
+    }
+}
